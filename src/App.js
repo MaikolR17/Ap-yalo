4358193b833edd9789c743f33dd0aee48122ebfc
Maikol Rodriguez <maikolrodriguezm@gmail.com>
2024-05-08 18:25:45 -0400

    Se creo un componente PersonCard y utilizando props se mostro cuatro componentes PersonCard que señalan a cuatro personas diferentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import PersonCard from './/Componentes/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard
+        name={"Doe"}
+        lastName={"Jane"}
+        age={"45"}
+        hairColor={"Black"}
+      />
+      <PersonCard
+        name={"Smith"}
+        lastName={"John"}
+        age={"88"}
+        hairColor={"Brown"}
+      />
+      <PersonCard
+        name={"Fillmore"}
+        lastName={"Millard"}
+        age={"50"}
+        hairColor={"Brown"}
+      />
+      <PersonCard
+        name={"Smith"}
+        lastName={"Maria"}
+        age={"62"}
+        hairColor={"Brown"}
+      />
     </div>
   );
 }
